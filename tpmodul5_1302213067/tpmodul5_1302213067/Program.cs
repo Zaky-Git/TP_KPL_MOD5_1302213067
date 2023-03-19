dcc95bf11577d1d73488aa97670943224ff35845
MUHAMMAD ZAKY FATHURAHIM <mzakyf5503@gmail.com>
2023-03-19 22:38:40 +0700

    fix konflik

--- tpmodul5_1302213067/tpmodul5_1302213067/Program.cs
+++ tpmodul5_1302213067/tpmodul5_1302213067/Program.cs
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 ﻿class HaloGeneric
 {
     public void SapaUser(string x)
     {
         Console.WriteLine("Halo user " + x);
-=======
+    }
+}
 ﻿class DataGeneric<T>
 {
     private T data;
@@ -16,7 +16,6 @@
     public void PrintData()
     {
         Console.WriteLine("Data yang tersimpan adalah: " + data);
->>>>>>> generic-class
     }
 }
 
@@ -24,14 +23,11 @@ class main
 {
     static void Main(string[] args)
     {
-<<<<<<< HEAD
         String inputan = Console.ReadLine();
         HaloGeneric halo = new HaloGeneric();
         halo.SapaUser(inputan);
-=======
 
         DataGeneric<String> data = new DataGeneric<string>("1302213067");
         data.PrintData();
->>>>>>> generic-class
     }
 }
