a3bea120a0e3733dedd8506edd18d47ca119c441
MUHAMMAD ZAKY FATHURAHIM <mzakyf5503@gmail.com>
2023-03-19 22:28:32 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302213067/tpmodul5_1302213067/Program.cs
+++ tpmodul5_1302213067/tpmodul5_1302213067/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class main
+{
+    static void Main(string[] args)
+    {
+
+        DataGeneric<String> data = new DataGeneric<string>("1302213067");
+        data.PrintData();
+    }
+}
